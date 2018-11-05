168fc4a80a6b87415875759c72af84e2553e9eef
Matthew Spiteri <matthew.spiteri.92m@gmail.com>
2018-11-05 23:08:37 +0100

    Add project files.

---
+++ NRulesTesting/DomainModels/Customer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NRulesTesting.DomainModels
+{
+    public class Customer
+    {
+        public string Name { get; private set; }
+        public bool IsPreferred { get; set; }
+
+        public Customer(string name)
+        {
+            Name = name;
+        }
+
+        public void NotifyAboutDiscount()
+        {
+            Console.WriteLine("Customer {0} was notified about a discount", Name);
+        }
+    }
+
+}
---
+++ NRulesTesting/Program.cs
@@ -0,0 +1,87 @@
+﻿using NRules;
+using NRules.Fluent;
+using NRules.Fluent.Dsl;
+using NRulesTesting.DomainModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NRulesTesting
+{
+    public class PreferredCustomerDiscountRule : Rule
+    {
+        public override void Define()
+        {
+            Customer customer = null;
+            IEnumerable<Order> orders = null;
+
+            When()
+                .Match<Customer>(() => customer, c => c.IsPreferred)
+                .Query(() => orders, x => x
+                    .Match<Order>(
+                        o =>o.Customer == customer,
+                        o => !o.IsDiscounted)
+                    .Collect()
+                    .Where(c => c.Any()));
+
+            Then()
+                .Do(ctx => ApplyDiscount(orders, 10.0))
+                .Do(ctx => ctx.UpdateAll(orders));
+        }
+
+        private static void ApplyDiscount(IEnumerable<Order> orders, double discount)
+        {
+            foreach (var order in orders)
+            {
+                order.ApplyDiscount(discount);
+            }
+        }
+    }
+
+    public class DicsountNotificationRule : Rule
+    {
+        public override void Define()
+        {
+            Customer customer = null;
+
+            When()
+                .Match<Customer>(() => customer)
+                .Exists<Order>(o => o.Customer == customer, o => o.PercentDiscount > 0.0);
+
+            Then()
+                .Do(_ => customer.NotifyAboutDiscount());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Load rules
+            var repository = new RuleRepository();
+            repository.Load(x => x.From(typeof(PreferredCustomerDiscountRule).Assembly));
+
+            //Compile rules
+            var factory = repository.Compile();
+
+            //Create a working session
+            var session = factory.CreateSession();
+
+            //Load domain model
+            var customer = new Customer("John Doe") { IsPreferred = true };
+            var order1 = new Order(123456, customer, 2, 25.0);
+            var order2 = new Order(123457, customer, 1, 100.0);
+
+            //Insert facts into rules engine's memory
+            session.Insert(customer);
+            session.Insert(order1);
+            session.Insert(order2);
+
+            //Start match/resolve/act cycle
+            session.Fire();
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
